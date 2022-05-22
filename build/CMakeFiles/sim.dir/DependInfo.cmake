0696ddb7f5197cdd531b12a070440f4dc2efa8f3
niraulapm <prashantniraula@gmail.com>
2022-05-22 19:48:36 -0400

    basic simulation-one particle created

---
+++ build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/prashant/Desktop/sim/action.cc" "/home/prashant/Desktop/sim/build/CMakeFiles/sim.dir/action.cc.o"
+  "/home/prashant/Desktop/sim/construction.cc" "/home/prashant/Desktop/sim/build/CMakeFiles/sim.dir/construction.cc.o"
+  "/home/prashant/Desktop/sim/generator.cc" "/home/prashant/Desktop/sim/build/CMakeFiles/sim.dir/generator.cc.o"
+  "/home/prashant/Desktop/sim/physics.cc" "/home/prashant/Desktop/sim/build/CMakeFiles/sim.dir/physics.cc.o"
+  "/home/prashant/Desktop/sim/sim.cc" "/home/prashant/Desktop/sim/build/CMakeFiles/sim.dir/sim.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_RAYTRACERX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/geant4/11.0.1/include/Geant4"
+  "/home/prashant/anaconda3/include/qt"
+  "/home/prashant/anaconda3/include/qt/QtGui"
+  "/home/prashant/anaconda3/include/qt/QtCore"
+  "/home/prashant/anaconda3/./mkspecs/linux-g++"
+  "/home/prashant/anaconda3/include/qt/QtOpenGL"
+  "/home/prashant/anaconda3/include/qt/QtWidgets"
+  "/home/prashant/anaconda3/include/qt/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
